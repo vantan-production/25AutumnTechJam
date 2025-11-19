641ac7eaa19ed141a7810cc4b3681466ce1e969a
sakurai <kanato25kdgn@gmail.com>
2025-11-19 19:57:40 +0900

    deat: リレーションをつなげた

--- back/app/Models/travelPlan.php
+++ back/app/Models/travelPlan.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class travelPlan extends Model
+class TravelPlan extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "budget",
+        "staying_time",
+        "stay_at",
+    ];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function waypoints() {
+        return $this->hasMany(Waypoint::class);
+    }
 }
