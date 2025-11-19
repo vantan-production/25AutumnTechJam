05be7ea5be6e76cac014b699b486f5d5ab1711d2
sakurai <kanato25kdgn@gmail.com>
2025-11-19 14:25:52 +0900

    feat: ショップとwaypointのテーブル作成

---
+++ back/database/migrations/2025_11_19_051919_create_waypoints_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('waypoints', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("shop_id");
+            $table->unsignedBigInteger("travel_plan_id");
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('waypoints');
+    }
+};
