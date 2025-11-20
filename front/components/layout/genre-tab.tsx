99b64eb6092462bc64d783fc3fe21a2de84060fa
shion <shion48691811@gmail.com>
2025-11-20 16:57:15 +0900

    click動作の実装

--- front/components/layout/genre-tab.tsx
+++ front/components/layout/genre-tab.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 function GenreTab() {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
   const genre = [
     "sweets",
     "sweets",
@@ -6,11 +12,20 @@ function GenreTab() {
     "sweets",
     "sweets",
     "sweets",
-  ].map((item) => (
-    <div className="w-14 h-11 bg-white radius-1 box-shadow flex items-center justify-center flex-shrink-0">
+  ].map((item, index) => (
+    <div
+      className="w-14 h-11 radius-1 box-shadow flex items-center justify-center flex-shrink-0 cursor-pointer transition-colors"
+      style={{
+        backgroundColor: selectedIndex === index ? "#050505" : "#ffffff",
+        color: selectedIndex === index ? "#ffffff" : "#050505",
+      }}
+      key={index}
+      onClick={() => setSelectedIndex(index === selectedIndex ? null : index)}
+    >
       {item}
     </div>
   ));
+
   return (
     <div className="w-[382px] h-13 mx-auto bg-beige radius-2 box-shadow flex items-center gap-2 px-2.5 left-1">
       <svg
@@ -30,7 +45,7 @@ function GenreTab() {
         />
       </svg>
       <div className="w-[0.7px] h-8 bg-black flex-shrink-0"></div>
-      <div className="flex gap-2 overflow-x-auto flex-1 min-w-0">{genre}</div>
+      <div className="flex gap-2 overflow-x-auto flex-2 min-w-0">{genre}</div>
     </div>
   );
 }
