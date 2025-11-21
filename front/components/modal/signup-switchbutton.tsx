3bbe68fbc832a376e1c24ad0baa9610cbefdec38
ito <yuma.24s1100609@gmail.com>
2025-11-21 17:32:58 +0900

    add signup変更ボタン作成

---
+++ front/components/modal/signup-switchbutton.tsx
@@ -0,0 +1,11 @@
+export function SignupSwitch() {
+    return (
+        <div className="w-14 mx-auto mt-4">
+            <button>
+                <p className="h4 underline ">Signup</p>
+            </button>
+        </div>
+    )
+}
+
+export default SignupSwitch;
