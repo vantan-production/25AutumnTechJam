58ad8090c44fc35664f7bff5c34e569de28b0431
shion <shion48691811@gmail.com>
2025-11-21 14:04:53 +0900

    一旦コミット

--- front/src/components/layout/filter.tsx
+++ front/src/components/layout/filter.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import "igniteui-webcomponents/themes/light/bootstrap.css";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import { useState } from "react";
 
 // 動的インポートで SSR を無効化
 const IgrRangeSlider = dynamic(
@@ -12,8 +13,11 @@ const IgrRangeSlider = dynamic(
 );
 
 export const Filter = () => {
+  const [defaultValues, setDefaultValues] = useState([0, 99]);
+  const minBudget = 0;
+  const maxBudget = 20000;
   return (
-    <div className="fixed bottom-0 bg-beige w-[393px] h-[474px] rounded-tl-[50px] rounded-tr-[50px]">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 bg-beige w-[393px] h-[474px] rounded-tl-[50px] rounded-tr-[50px]">
       <div className="flex justify-center items-center">
         <div className="bg-black w-[138px] h-[3px] mt-[8.5px]"></div>
       </div>
@@ -63,25 +67,39 @@ export const Filter = () => {
         </div>
       </div>
       <div className="text-black mt-12 ml-11 text-xl">Budget</div>
-      <Slider
-        className="rc-slider"
-        range
-        defaultValue={[0, 99]}
-        min={0}
-        max={99}
-        onChangeComplete={() => {}}
-        onChange={() => {}}
-        styles={{
-          handle: {
-            height: 20,
-            width: 20,
-            marginTop: "-8px",
-            opacity: 1,
-          },
-          rail: { height: 5 },
-        }}
-      />
-      ;
+      <div className="flex items-center justify-between w-[329px] mx-auto">
+        <div className="w-fit">{minBudget}</div>
+        <div className="w-fit">{maxBudget}</div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Slider
+          className="rc-slider"
+          range
+          defaultValue={[0, 99]}
+          min={0}
+          max={20000}
+          onChangeComplete={() => {}}
+          styles={{
+            handle: {
+              height: 20,
+              width: 20,
+              marginTop: "-8px",
+              opacity: 1,
+            },
+            rail: { height: 5 },
+          }}
+          value={defaultValues}
+          onChange={(value) => setDefaultValues(value as number[])}
+        />
+      </div>
+      <div className="flex items-center justify-start w-[329px] gap-2 mx-auto">
+        <p>¥</p>
+        <div className="w-3 bg-white radius-3 box-shadow-1">
+          {defaultValues[0]}
+        </div>
+        <p>~</p>
+        <div>{defaultValues[1]}</div>
+      </div>
     </div>
   );
 };
