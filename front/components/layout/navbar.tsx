902e8d588da817ca7dbe60bb01d65098daab1172
ito <yuma.24s1100609@gmail.com>
2025-11-21 11:25:39 +0900

    update カラー、配置変更

--- front/components/layout/navbar.tsx
+++ front/components/layout/navbar.tsx
@@ -1,13 +1,29 @@
 "use client";   // これが先頭にあるか確認
+import { useState } from "react";
 
 export function TabBar () {
+    const [active, setActive] = useState(1);
+
     return (
-        <div className="flex bg-beige w-80 h-14 m-auto rounded-lg">
-            <img className="w-8 h-8" src="images/coffeeShopInner.png" alt="home" />
-            <div>
-                <img className="w-8 h-8" src="images/mapInner.png" alt="map" />
-            </div>
-            <img className="w-8 h-8" src="images/bookmarkInner.png" alt="keep" />
+        <div className="flex bg-beige w-80 h-14 m-auto rounded-lg justify-between">
+                <button 
+                onClick={() => setActive(1)}
+                className={`w-28 h-12 my-1 mx-1.5 rounded-md ${active === 1 ? "bg-white shadow-md" : "bg-beige shadow-none"}`}
+                >
+                    <img className="w-8 h-8 m-auto" src="images/coffeeShopInner.png" alt="home" />
+                </button>
+                <button 
+                onClick={() => setActive(2)}
+                className={`w-28 h-12 my-1 mx-1.5 rounded-md ${active === 2 ? "bg-white shadow-md" : "bg-beige shadow-none"}`}
+                >
+                    <img className="w-8 h-8 m-auto" src="images/mapInner.png" alt="map" />
+                    </button>
+                <button 
+                onClick={() => setActive(3)}
+                className={`w-28 h-12 my-1 mx-1.5 rounded-md ${active === 3 ? "bg-white shadow-md" : "bg-beige shadow-none"}`}
+                >
+                    <img className="w-8 h-8 m-auto" src="images/bookmarkInner.png" alt="keep" />
+                </button>
         </div>
     )
 }
