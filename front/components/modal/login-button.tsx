eec41fcee62432d1a4540ad965ef8f7870e6c8a6
ito <yuma.24s1100609@gmail.com>
2025-11-21 16:37:26 +0900

    add ログインボタン作成

---
+++ front/components/modal/login-button.tsx
@@ -0,0 +1,9 @@
+export function LoginButton () {
+    return (
+        <div className="w-32 mx-auto mt-4">
+            <button className="bg-green h-12 w-32 radius-2">Login</button>
+        </div>
+    )
+}
+
+export default LoginButton;
