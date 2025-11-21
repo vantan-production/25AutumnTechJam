302612017b373b75be93a38f19d96a54ae3a7232
ito <yuma.24s1100609@gmail.com>
2025-11-21 16:22:02 +0900

    add パスワード入力欄作成

---
+++ front/components/modal/modal-password.tsx
@@ -0,0 +1,14 @@
+export function ModalPass () {
+    return (
+        <div className="flex border-2 border-green w-60 h-10 mt-4 mx-auto rounded-md">
+            <img className="w-6 h-6 my-auto mx-2"src="images/lock.png" alt="lock" />
+            <input 
+            type="text"
+            placeholder="Password"
+            className="p w-full"
+            />
+        </div>
+    )
+}
+
+export default ModalPass;
