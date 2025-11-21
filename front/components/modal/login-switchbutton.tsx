4cf33d1262911122bd1d416fe67df6915b4640f5
ito <yuma.24s1100609@gmail.com>
2025-11-21 17:38:17 +0900

    add login変更ボタン作成

---
+++ front/components/modal/login-switchbutton.tsx
@@ -0,0 +1,11 @@
+export function LoginSwitch () {
+    return(
+        <div className="w-14 mx-auto mt-4">
+            <button>
+                <p className="h4 underline">Login</p>
+            </button>
+        </div>
+    )
+}
+
+export default LoginSwitch;
