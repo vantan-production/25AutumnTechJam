85261fe509f99ae6ceddea5c57b396f497e99447
kondo <2soshiro24@gmail.com>
2025-11-21 10:33:14 +0900

    詳細ページのタグを追加

--- front/src/app/top/page.tsx
+++ front/src/app/top/page.tsx
@@ -1,15 +1,18 @@
 import Bookmark from "../../../components/ui/bookmark";
 import Card from "../../../components/features/card";
+import DetailHead from "../../../components/features/shop-detail-head";
+import Tag from "../../../components/ui/tag";
 
 function Page() {
     return (
         <div>
-            <Card
+            {/*<Card
             shopName="shop-name"
             businessHours="8:00~18:00"
             parse="¥800~2,000"
             explanation="This shop is beautiful and traditional."
-            ></Card>
+            ></Card>*/}
+            <DetailHead shopName="tentaCafe" tag={['itarian','sweets','bakery','American']}></DetailHead>
         </div>
 
     )
