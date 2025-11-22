650b2825222d1db50cf72af6cdf00cdcad6d9e23
kondo <2soshiro24@gmail.com>
2025-11-23 00:03:50 +0900

    不要な記号削除＆モーダル遷移実装準備

--- front/src/app/test/page.tsx
+++ front/src/app/test/page.tsx
@@ -3,11 +3,13 @@ import Header from "../../../components/layout/header";
 
 import Page from "../top-page/page";
 import LoginModal from "../../../components/modal/loginModal";
+import TabBar from "../../../components/layout/navbar";
 
 export default function TestPage() {
   return (
     <div>
       <LoginModal></LoginModal>
+      <TabBar></TabBar>
     </div>
   );
 }
