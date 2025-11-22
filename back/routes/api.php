ee9e751524e29d6b9d990cc8e137e8f777f3deea
sakurai <kanato25kdgn@gmail.com>
2025-11-22 20:31:13 +0900

    fix: api.phpのコンフリクトの修正

--- back/routes/api.php
+++ back/routes/api.php
@@ -23,6 +23,5 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 Route::post('/travel_plan/{id}/del',[TravelPlansTableController::class,'index']);
 
-Route::get('/way_point', [WaypointTableController::class, 'index']);
 Route::post('/way_point/store',[WaypointTableController::class,'index']);
 Route::post('/way_point/{id}/del',[WaypointTableController::class,'index']);
